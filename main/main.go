ca79f2f53b65faa0c0824567542e9e636e594f67
zhoujian <123456>
2019-07-16 14:21:56 +0800

    go big.float包处理数据

--- main/main.go
+++ main/main.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"math/big"
 	"sync"
 )
 
 func main() {
 
-	for i := 0;i < 10000;i ++ {
-		go appendValueNoMutex(i)	// 不加锁会丢失
-		//go appendValueWithMutex(i)	// 加锁不会丢失
-	}
-	//time.Sleep(time.Second)
-	sort.Ints(s)
-	for i,v := range s{
-		fmt.Println(i,":",v)
-	}
+	a1 := ToBigFloat("100.234aj123")
+	fmt.Println(a1)
+	a2 := big.NewFloat(-100)
+
+
+	fmt.Printf("sum ===  %s\n", a1.Add(a1,a2).String())
+	//fmt.Printf("a2 = %s\n", a2.String())
+
+	//for i := 0;i < 10000;i ++ {
+	//	go appendValueNoMutex(i)	// 不加锁会丢失
+	//	//go appendValueWithMutex(i)	// 加锁不会丢失
+	//}
+	////time.Sleep(time.Second)
+	//sort.Ints(s)
+	//for i,v := range s{
+	//	fmt.Println(i,":",v)
+	//}
+}
+func ToBigFloat(str string) *big.Float {
+	f, _, _ := big.ParseFloat(str, 10, 256, big.ToNearestEven)
+	return f
 }
 
 var s []int
