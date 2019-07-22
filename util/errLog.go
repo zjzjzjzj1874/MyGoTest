3656873002d12a881469076029320f3ea392ca47
zhoujian <123456>
2019-07-22 16:03:38 +0800

    简单日志记录

--- util/errLog.go
+++ util/errLog.go
@@ -1,15 +1,13 @@
-package log
+// 简单的日志打印方法
+package util
 
 import (
-	"MyGoTest/util"
-	"encoding/json"
 	"fmt"
 	"os"
-	"strconv"
 	"time"
 )
 
-// 写错误log
+// 错误日志记录
 func ErrorLog(i... interface{}) {
 	path := "./log/errLog" + time.Now().Format("20060102") + ".log"
 	if !isExist(path){
@@ -18,10 +16,10 @@ func ErrorLog(i... interface{}) {
 	fMode := os.O_APPEND|os.O_WRONLY
 	f, _ := os.OpenFile(path,fMode,0666)
 	defer f.Close()
-	_,err := f.WriteString(time.Now().Format("2006-01-02 15:04:05")+ " [ERROR] | " + util.InterfaceToString(i) + "\n")
+	_,err := f.WriteString(time.Now().Format("2006-01-02 15:04:05")+ " [ERROR] | " + InterfaceToString(i) + "\n")
 	fmt.Println(err)
 }
-// 写普通打印log
+// 普通日志记录
 func InfoLog(i... interface{}) {
 	path := "./log/infoLog" + time.Now().Format("20060102") + ".log"
 	if !isExist(path){
@@ -30,26 +28,8 @@ func InfoLog(i... interface{}) {
 	fMode := os.O_APPEND|os.O_WRONLY
 	f, _ := os.OpenFile(path,fMode,0666)
 	defer f.Close()
-	_,err := f.WriteString(time.Now().Format("2006-01-02 15:04:05")+ " [INFO] | " + util.InterfaceToString(i) + "\n")
+	_,err := f.WriteString(time.Now().Format("2006-01-02 15:04:05")+ " [INFO] | " + InterfaceToString(i) + "\n")
 	fmt.Println(err)
-}
-//将可能是int\int32\int64\float64\string类型的interface{}转换成string，可继续完善类型
-func interfaceToString(i interface{}) string {
-	switch i.(type) {
-	case int:
-		return strconv.Itoa(i.(int))
-	case int32:
-		return strconv.Itoa(int(i.(int32)))
-	case int64:
-		return strconv.FormatInt(i.(int64), 10)
-	case float64:
-		return strconv.FormatFloat(i.(float64), 'f', -1, 64)
-	case string:
-		return i.(string)
-	default:
-		str,_ := json.Marshal(i)
-		return string(str)
-	}
 }
 // 创建文件
 func create(path string) interface{} {
