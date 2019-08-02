e9c8e15214a1f33203fab2f878605e0ceff328bb
zhoujian <123456>
2019-08-02 16:28:18 +0800

    日志记录变化

--- util/errLog.go
+++ util/errLog.go
@@ -2,46 +2,94 @@
 package util
 
 import (
-	"fmt"
 	"os"
+	"strconv"
+	"sync"
 	"time"
 )
 
+var (
+	lock sync.Mutex
+	Config *LogConfig
+)
+const (
+	InitVersion		= iota
+	LogMaxSize		= 200	// 单个文件最大20M
+	ErrLogPath		= "./log/errLog"	// TODO 读取配置文件
+	InfoLogPath		= "./log/infoLog"	// TODO 读取配置文件
+	Suffix			= ".log"			// TODO 读取配置文件
+)
+// 日志类型
+const(
+	LogTypeInfo = iota	// 普通日志
+	LogTypeError		// 错误日志
+)
+
+type LogConfig struct{
+	LogSize		int		// 日志大小
+	LogSuffix	string	// 日志后缀
+	LogPath		string	// 日志路径
+	LogType		int		// 日志类型
+	LogDate		string	// 日志日期
+	LogVersion	int		// 日志版本
+}
+
+func init()  {
+	Config = new(LogConfig)
+	Config.LogSuffix = Suffix
+	Config.LogDate = time.Now().Format("2006-01-02 15:04:05")
+	Config.LogVersion = InitVersion
+}
+
 // 错误日志记录
-func ErrorLog(i... interface{}) {
-	path := "./log/errLog" + time.Now().Format("20060102") + ".log"
-	if !isExist(path){
-		create(path)
-	}
+func (l *LogConfig) Error(i... interface{}) {
+	l.returnFileName("./log/errLog")
 	fMode := os.O_APPEND|os.O_WRONLY
-	f, _ := os.OpenFile(path,fMode,0666)
+	lock.Lock()
+	f, _ := os.OpenFile(Config.LogPath,fMode,0666)
 	defer f.Close()
-	_,err := f.WriteString(time.Now().Format("2006-01-02 15:04:05")+ " [ERROR] | " + InterfaceToString(i) + "\n")
-	fmt.Println(err)
+	_,_ = f.WriteString(time.Now().Format("2006-01-02 15:04:05")+ " [ERROR] | " + InterfaceToString(i) + "\n")
+	lock.Unlock()
 }
+
 // 普通日志记录
-func InfoLog(i... interface{}) {
-	path := "./log/infoLog" + time.Now().Format("20060102") + ".log"
-	if !isExist(path){
-		create(path)
-	}
+func (l *LogConfig) Info(i... interface{}) {
+	l.returnFileName("./log/infoLog")
 	fMode := os.O_APPEND|os.O_WRONLY
-	f, _ := os.OpenFile(path,fMode,0666)
+	lock.Lock()
+	f, _ := os.OpenFile(Config.LogPath,fMode,0666)
 	defer f.Close()
-	_,err := f.WriteString(time.Now().Format("2006-01-02 15:04:05")+ " [INFO] | " + InterfaceToString(i) + "\n")
-	fmt.Println(err)
+	_,_ = f.WriteString(time.Now().Format("2006-01-02 15:04:05")+ " [INFO] | " + InterfaceToString(i) + "\n")
+	lock.Unlock()
 }
+
+// 创建文件,判断大小,返回文件名
+func (l *LogConfig) returnFileName(logPath string) {
+	switch {
+	case logPath == ErrLogPath:
+		logPath = ErrLogPath
+	default:
+		logPath = InfoLogPath
+	}
+	Config.LogPath = logPath + Config.LogDate + "V" + strconv.Itoa(Config.LogVersion) + Config.LogSuffix
+	if !l.isExist(){
+		_ = l.create()
+	}else{
+		// 已经存在,就去判断文件大小
+	}
+}
+
 // 创建文件
-func create(path string) interface{} {
-	_, err := os.Create(path)
+func (l *LogConfig) create() error {
+	_, err := os.Create(Config.LogPath)
 	if err != nil {
 		return err
 	}else{
-		return true
+		return nil
 	}
 }
 // 文件（夹）是否存在
-func isExist(filePath string) bool {
-	_, err := os.Stat(filePath)
+func (l *LogConfig) isExist() bool {
+	_, err := os.Stat(Config.LogPath)
 	return err == nil || os.IsExist(err)
 }
--- util/errLog_test.go
+++ util/errLog_test.go
@@ -1,11 +1,16 @@
 package util
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestErrorLog(t *testing.T) {
+	ErrorLog("测试添加","123","456")
 	ErrorLog("测试添加","123","456")
 }
 
 func TestInfoLog(t *testing.T) {
-	InfoLog("我","hello world")
+	//InfoLog("我","hello world")
+	fmt.Println(Config.LogSuffix)
 }
