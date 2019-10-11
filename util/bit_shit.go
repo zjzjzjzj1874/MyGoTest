9d7871d49f640605f485d7120e57b2c89f0dda7b
zhoujian <123456>
2019-10-11 13:40:02 +0800

    位左右移动

---
+++ util/bit_shit.go
@@ -0,0 +1,30 @@
+package util
+
+import "fmt"
+
+// 位移动
+func LeftShift() {
+	const (
+		a = 1 << iota // 1 << 0
+		b             // 1 << 1
+		c             // 1 << 2
+		d             // 1 << 3
+	)
+	fmt.Println(a) // 1
+	fmt.Println(b) // 2
+	fmt.Println(c) // 4
+	fmt.Println(d) // 8
+}
+
+func RightShift() {
+	const (
+		a = 16 >> iota // 16 >> 0
+		b              // 16 >> 1
+		c              // 16 >> 2
+		d              // 16 >> 3
+	)
+	fmt.Println(a) // 16
+	fmt.Println(b) // 8
+	fmt.Println(c) // 4
+	fmt.Println(d) // 2
+}
---
+++ util/bit_shit_test.go
@@ -0,0 +1,11 @@
+package util
+
+import "testing"
+
+func TestLeftShift(t *testing.T) {
+	LeftShift()
+}
+
+func TestRightShift(t *testing.T) {
+	RightShift()
+}
