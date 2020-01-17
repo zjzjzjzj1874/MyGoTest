6f9757643846475a8b0dc96e3362cf3044618c7e
zhoujian <123456>
2020-01-17 14:46:02 +0800

    go学习
    (cherry picked from commit 7aaf23aafa4bdfbd4b55417a52a91c30c1419be9)

---
+++ util/arithmetic/merge_sort.go
@@ -0,0 +1,30 @@
+// 归并排序  https://baike.baidu.com/item/归并排序/1639015?fr=aladdin
+// https://www.jianshu.com/p/33cffa1ce613
+package arithmetic
+
+func mergeSort(r []int) []int {
+	length := len(r)
+	if length <= 1 {
+		return r
+	}
+	num := length / 2
+	left := mergeSort(r[:num])
+	right := mergeSort(r[num:])
+	return merge(left, right)
+}
+
+func merge(left, right []int) (result []int) {
+	l, r := 0, 0
+	for l < len(left) && r < len(right) {
+		if left[l] < right[r] {
+			result = append(result, left[l])
+			l++
+		} else {
+			result = append(result, right[r])
+			r++
+		}
+	}
+	result = append(result, left[l:]...)
+	result = append(result, right[r:]...)
+	return
+}
---
+++ util/arithmetic/merge_sort_test.go
@@ -0,0 +1,10 @@
+package arithmetic
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMergeSort(t *testing.T) {
+	fmt.Println(mergeSort([]int{1, 5, 2, 8, 3, 9, 0, 4, 7}))
+}
