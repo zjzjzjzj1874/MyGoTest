1d786ff8917b8c6bb401d8e5abf56f3846617166
zhoujian <123456>
2020-03-13 11:47:25 +0800

    正则规则添加学习
    (cherry picked from commit 7aaf23aafa4bdfbd4b55417a52a91c30c1419be9)

--- util/RregExp/regexp.go
+++ util/RregExp/regexp.go
@@ -10,22 +10,46 @@ import (
 // if the checkType contains "1",the STR can contains 1-9;
 // if the checkType contains "a",the STR can contains a-z;
 // if the checkType contains "A",the STR can contains A-Z;
-func CheckNumberAndLetter(str, checkType string) bool{
+func CheckNumberAndLetter(str, checkType string) bool {
 	reg := "^["
-	if strings.Contains(checkType,"1"){
+	if strings.Contains(checkType, "1") {
 		reg += "0-9"
 	}
-	if strings.Contains(checkType,"a"){
+	if strings.Contains(checkType, "a") {
 		reg += "a-z"
 	}
-	if strings.Contains(checkType,"A"){
+	if strings.Contains(checkType, "A") {
 		reg += "A-Z"
 	}
 	reg += "]$"
-	for i := 0;i < len(str);i ++ {
-		if !regexp.MustCompile(reg).MatchString(string(str[i])){
+	for i := 0; i < len(str); i++ {
+		if !regexp.MustCompile(reg).MatchString(string(str[i])) {
 			return false
 		}
 	}
 	return true
+}
+
+// 正则匹配电话 简单匹配
+func IsPhoneNumber(phone string) bool {
+	return regexp.MustCompile(`^1((45|47)|[356789]\d)\d{8}$`).MatchString(phone)
+}
+
+// 正则匹配密码
+func IsCorrectPassword(password string) bool {
+	PasswordRex := regexp.MustCompile(`^[\w+=-]{6,20}$`)
+	return PasswordRex.MatchString(password)
+}
+
+// 正则匹配身份证 (二代身份证验证)
+func IsCorrectIdCard(idCard string) bool {
+	PasswordRex := regexp.MustCompile(`^[1-9]\d{5}(((1[89]|20)\d{2}(((0[13578]|1[0-2])(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)(0[1-9]|[12][0-9]|30))|(02(0[1-9]|[1][0-9]|2[0-8]))))|((((1[89]|20)(0[48]|[2468][048]|[13579][26]))|((19|20)00))0229))\d{3}(\d|X|x)$`)
+	return PasswordRex.MatchString(idCard)
+}
+
+// 银行卡号简单匹配 注:严格匹配可以用支付宝接口
+// 我国银行卡片主要分两种——借记卡（普通储蓄卡）和信用卡（贷记卡）。借记卡的卡号一般是19位，但有例外：招行16位，交行17位；信用卡的卡号一般是16位。
+func IsCorrectBankNo(bankNo string) bool {
+	bankNoRex := regexp.MustCompile(`^([1-9])(\d{15}|\d{16}|\d{18})$`)
+	return bankNoRex.MatchString(bankNo)
 }
--- util/RregExp/regexp_test.go
+++ util/RregExp/regexp_test.go
@@ -6,11 +6,49 @@ import (
 )
 
 func TestCheckNumberAndLetter(t *testing.T) {
-	fmt.Println(CheckNumberAndLetter("Hello","1aA"))
-	fmt.Println(CheckNumberAndLetter("Hello","a"))
-	fmt.Println(CheckNumberAndLetter("Hello","1"))
-	fmt.Println(CheckNumberAndLetter("Hello","A"))
-	fmt.Println(CheckNumberAndLetter("alhjebfjb","a"))
-	fmt.Println(CheckNumberAndLetter("1236542342","1"))
-	fmt.Println(CheckNumberAndLetter("2983333aweaswdva907YUVuyb","1aA"))
+	fmt.Println(CheckNumberAndLetter("Hello", "1aA"))
+	fmt.Println(CheckNumberAndLetter("Hello", "a"))
+	fmt.Println(CheckNumberAndLetter("Hello", "1"))
+	fmt.Println(CheckNumberAndLetter("Hello", "A"))
+	fmt.Println(CheckNumberAndLetter("alhjebfjb", "a"))
+	fmt.Println(CheckNumberAndLetter("1236542342", "1"))
+	fmt.Println(CheckNumberAndLetter("2983333aweaswdva907YUVuyb", "1aA"))
+}
+
+func TestIsPhoneNumber(t *testing.T) {
+	fmt.Println(IsPhoneNumber("1523"))
+	fmt.Println(IsPhoneNumber("1552548721"))
+	fmt.Println(IsPhoneNumber("14525487212"))
+	fmt.Println(IsPhoneNumber("14725487212"))
+	fmt.Println(IsPhoneNumber("14325487212"))
+	fmt.Println(IsPhoneNumber("15525487212"))
+	fmt.Println(IsPhoneNumber("18725487212"))
+}
+
+func TestIsCorrectPassword(t *testing.T) {
+	fmt.Println(IsCorrectPassword(""))
+	fmt.Println(IsCorrectPassword("1as2"))
+	fmt.Println(IsCorrectPassword("1as22341"))
+	fmt.Println(IsCorrectPassword("Aas22341"))
+	fmt.Println(IsCorrectPassword("Aas221"))
+	fmt.Println(IsCorrectPassword("Aas周21"))
+	fmt.Println(IsCorrectPassword("Aas22"))
+}
+
+func TestIsCorrectIdCrad(t *testing.T) {
+	fmt.Println(IsCorrectIdCard("036767199903037878")) // 0开头
+	fmt.Println(IsCorrectIdCard("130981199312253466"))
+	fmt.Println(IsCorrectIdCard("130981200002293466")) // 2月29日出生
+	fmt.Println(IsCorrectIdCard("130981200002303466")) // 2月30日出生
+	fmt.Println(IsCorrectIdCard("13098119931225346x"))
+	fmt.Println(IsCorrectIdCard("13098119931225346")) // 17位
+}
+
+func TestIsCorrectBankNo(t *testing.T) {
+	fmt.Println(IsCorrectBankNo("1234567890123456789")) // 19位
+	fmt.Println(IsCorrectBankNo("123456789012345678"))  // 18位
+	fmt.Println(IsCorrectBankNo("12345678901234567"))   // 17位
+	fmt.Println(IsCorrectBankNo("1234567890123456"))    // 16位
+	fmt.Println(IsCorrectBankNo("123456789012345"))     // 15位
+	fmt.Println(IsCorrectBankNo("0309811993122534"))    // 0开头16位
 }
